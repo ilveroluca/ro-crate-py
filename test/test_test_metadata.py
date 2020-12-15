0ee51e17787aeac770e793cd63282a4b44316534
simleo <simleo@crs4.it>
2020-12-15 15:56:16 +0100

    refactor test entities; delete test service and engine

--- test/test_test_metadata.py
+++ test/test_test_metadata.py
@@ -16,18 +16,17 @@
 # limitations under the License.
 
 from rocrate.rocrate import ROCrate
-from rocrate.model.testservice import TestService
 from rocrate.model.testinstance import TestInstance
 from rocrate.model.testdefinition import TestDefinition
 from rocrate.model.testsuite import TestSuite
-from rocrate.model.testengine import TestEngine
 
 # Tell pytest these are not test classes (so it doesn't try to collect them)
-TestService.__test__ = False
 TestInstance.__test__ = False
 TestDefinition.__test__ = False
 TestSuite.__test__ = False
-TestEngine.__test__ = False
+
+JENKINS = "https://w3id.org/ro/terms/test#JenkinsService"
+PLANEMO = "https://w3id.org/ro/terms/test#PlanemoEngine"
 
 
 def test_read(test_data_dir, helpers):
@@ -41,36 +40,22 @@ def test_read(test_data_dir, helpers):
     assert wf_prop['@id'] == main_wf.id
     assert set(wf_prop['@type']) == helpers.WORKFLOW_TYPES
 
-    test_service = crate.dereference("#jenkins")
-    assert test_service.id == "#jenkins"
-    assert test_service.type == "TestService"
-    assert test_service.name == "Jenkins"
-    assert test_service.url == "https://www.jenkins.io"
-    assert test_service.testServiceType == "jenkins"
-
     test_instance = crate.dereference("#test1_1")
     assert test_instance.id == "#test1_1"
     assert test_instance.type == "TestInstance"
     assert test_instance.name == "test1_1"
     assert test_instance.url == "http://example.org/jenkins"
     assert test_instance.resource == "job/tests/"
-    assert test_instance.runsOn is test_service
-    assert test_instance.service is test_service
-
-    test_engine = crate.dereference("#planemo")
-    assert test_engine.id == "#planemo"
-    assert test_engine.type == "TestEngine"
-    assert test_engine.name == "Planemo"
-    assert test_engine.url == "https://github.com/galaxyproject/planemo"
-    assert test_engine.version == ">=0.70"
-    assert test_engine.testEngineType == "planemo"
+    assert test_instance.runsOn == JENKINS
+    assert test_instance.service == JENKINS
 
     def_id = "test/test1/sort-and-change-case-test.yml"
     test_definition = crate.dereference(def_id)
     assert test_definition.id == def_id
     assert set(test_definition.type) == {"File", "TestDefinition"}
-    assert test_definition.conformsTo is test_engine
-    assert test_definition.engine is test_engine
+    assert test_definition.conformsTo == PLANEMO
+    assert test_definition.engine == PLANEMO
+    assert test_definition.engineVersion == ">=0.70"
 
     test_suite = crate.dereference("#test1")
     assert test_suite.id == "#test1"
@@ -92,47 +77,25 @@ def test_read(test_data_dir, helpers):
 def test_create():
     crate = ROCrate()
 
-    test_service = TestService(crate, identifier="#jenkins")
-    crate._add_context_entity(test_service)
-    test_service.name = "Jenkins"
-    test_service.url = {"@id": "https://www.jenkins.io"}
-    test_service.testServiceType = "jenkins"
-    assert test_service.name == "Jenkins"
-    assert test_service.url == "https://www.jenkins.io"
-    assert test_service.testServiceType == "jenkins"
-
     test_instance = TestInstance(crate, identifier="#foo_instance_1")
     crate._add_context_entity(test_instance)
     test_instance.name = "Foo Instance 1"
     test_instance.url = {"@id": "http://example.org/foo"}
     test_instance.resource = "job/foobar"
-    test_instance.runsOn = test_service
+    test_instance.runsOn = {"@id": JENKINS}
     assert test_instance.name == "Foo Instance 1"
     assert test_instance.url == "http://example.org/foo"
     assert test_instance.resource == "job/foobar"
-    assert test_instance.runsOn is test_service
-    test_instance.runsOn = None
-    test_instance.service = test_service
-    assert test_instance.service is test_service
-
-    test_engine = TestEngine(crate, identifier="#planemo")
-    crate._add_context_entity(test_engine)
-    test_engine.name = "Planemo"
-    test_engine.url = {"@id": "https://github.com/galaxyproject/planemo"}
-    test_engine.version = ">=0.70"
-    test_engine.testEngineType = "planemo"
-    assert test_engine.name == "Planemo"
-    assert test_engine.url == "https://github.com/galaxyproject/planemo"
-    assert test_engine.version == ">=0.70"
-    assert test_engine.testEngineType == "planemo"
+    assert test_instance.runsOn == JENKINS
+    assert test_instance.service == JENKINS
 
     test_definition = TestDefinition(crate, dest_path="test/foo/bar.yml")
     crate._add_context_entity(test_definition)
-    test_definition.conformsTo = test_service
-    assert test_definition.conformsTo is test_service
-    test_definition.conformsTo = None
-    test_definition.engine = test_engine
-    assert test_definition.engine is test_engine
+    test_definition.conformsTo = {"@id": PLANEMO}
+    test_definition.engineVersion = ">=0.70"
+    assert test_definition.conformsTo == PLANEMO
+    assert test_definition.engine == PLANEMO
+    assert test_definition.engineVersion == ">=0.70"
 
     test_suite = TestSuite(crate, "#foosuite")
     crate._add_data_entity(test_suite)
